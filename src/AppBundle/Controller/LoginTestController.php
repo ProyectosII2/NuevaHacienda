482ed0a2462e2f8f17a2ed59830baba3c0ba5c08
bar-julio <bar.julio712@gmail.com>
2017-10-20 14:27:34 -0600

    Login Attempt 2
    Redirect Succesful

--- src/AppBundle/Controller/LoginTestController.php
+++ src/AppBundle/Controller/LoginTestController.php
@@ -8,34 +8,44 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Bundle\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 
 class LoginTestController extends Controller
 {
     /**
-     * @Route("/login")
+     * @Route("/failure",name="failure")
+     */
+    public function loadFailure(Request $request)
+    {
+        return $this->render('logintest/failure.html.twig');
+    }
+    /**
+     * @Route("/success",name="success")
+     */
+    public function loadSuccess(Request $request)
+    {
+        $username = "default";
+        $password = "default";
+        //$username = $param[0];
+        //$password = $param[1];
+        
+        dump($request);
+        return $this->render('logintest/success.html.twig', array ('username' => $username, 'password' => $password));
+    }
+    /**
+     * @Route("/login", name="login")
      */
     public function loginAction(Request $request)
     {
-       /* $form = $this->createFormBuilder()
-            ->add('Username')
-            ->add('Submit', SubmitType::class)
-            ->getForm();
-        return $this->render('::base.html.twig', [
-            'form'=> $form->CreateView(),
-            ]);
-            */
-        dump($request);
-            
-        return $this->render('logintest/login.html.twig');
-    }
-    /**
-     * @Route("/result")
-     */
-    public function postReturn(Request $request)
-    {
-        $username = $request->request->get('username');
-        $password = $request->request->get('pass');
-        
-        return $this->render('logintest/credentials.html.twig', array ('username' => $username, 'password' => $password));
+        if($request->request->has('username') && $request->request->has('pass'))
+        {
+            $vect = array('username'=>$request->request->get('username'),'password'=>$request->request->get('pass'));
+            //Hay post
+            return $this->redirectToRoute("success", $vect);
+
+        } else{
+            //NO hay Post
+            return $this->render('logintest/login.html.twig');
+        }
     }
 }
