8d906b72895d9501d32b7a47e460718d3c7bd794
Ludwing Cano <ludwing238@gmail.com>
2017-10-22 19:40:03 -0600

    Dashboard
    Creación de dashboard para landing

---
+++ src/AppBundle/Controller/DashboardController.php
@@ -0,0 +1,20 @@
+<?php
+
+//src/AppBundle/Controller/Ds.php
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+
+class DashboardController extends Controller
+{
+    /**
+     * @Route("/dash")
+     */
+    public function Mostrar()
+    {       
+        return $this->render('vistas/dashboard.html.twig');
+    }
+
+}
