f16036cbb4b03c19334725e94bc415aee03b009b
bar-julio <bar.julio712@gmail.com>
2017-10-22 19:16:51 -0600

    Login con vista
    Ya se hizó el login con usuario en memoria

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -10,7 +10,7 @@ use Symfony\Component\HttpFoundation\Request;
 class DefaultController extends Controller
 {
     /**
-     * @Route("/", name="homepage")
+     * @Route("/welcome", name="welcome")
      */
     public function indexAction(Request $request)
     {
--- src/AppBundle/Controller/LoginController.php
+++ src/AppBundle/Controller/LoginController.php
@@ -6,49 +6,42 @@ namespace AppBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
+use Symfony\Component\HttpFoundation\Session\Session;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+//$session = new Session();
+//$session->start();
 
-class LoginTestController extends Controller
+
+class LoginController extends Controller
 {
     /**
-     * @Route("/failure",name="failure")
-     */
-    public function loadFailure(Request $request)
-    {
-        return $this->render('logintest/failure.html.twig');
-    }
-    /**
-     * @Route("/success",name="success")
+     * @Route("/",name="homepage")
      * @param Request $request
-     * @Security("has_role('ROLE_ADMIN')")
-     * 
+     * @Security("has_role('ROLE_ADMIN') or has_role('ROLE_USER')") 
      */
-    public function loadSuccess(Request $request)
+    public function loadLanding(Request $request)
     {
-        $username = "default";
-        $password = "default";
-        //$username = $param[0];
-        //$password = $param[1];
-        
-        dump($request);
-        return $this->render('logintest/success.html.twig', array ('username' => $username, 'password' => $password));
+        //$lastusername -> getName();
+        $lastusername = 
+        $lastusername = $this->get('security.token_storage')->getToken()->getUser();
+        return $this->render('logintest/success.html.twig', 
+            array ('username' => $lastusername, 'variable' => $_SESSION['variable']
+        ));
     }
     /**
      * @Route("/login", name="login")
      * 
      */
-    public function loginAction(Request $request, AuthenticationUtils $utils)
+    public function loginAction(Request $request)
     {
-        dump($utils);
-        $error = $utils->getLastAuthenticationError();
-        $lastUsername = $utils->getLastUsername();
-
+        $_SESSION['variable'] = "esta es una variable de sesion";
         return $this->render('vistas/login.html.twig');
 
         /*
+        $session->set('name', $request->request->get('_username'));
         if($request->request->has('username_parameter') && $request->request->has('password_parameter'))
         {
             //$vect = array('username'=>$request->request->get('username_u'),'password'=>$request->request->get('pass'));
