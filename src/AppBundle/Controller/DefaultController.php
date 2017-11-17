b909afd6da05b816682b414b3c472dd0f6b574b3
bar-julio <bar.julio712@gmail.com>
2017-11-17 00:04:54 -0600

    Variables de Session 2
    cargando desde controlador

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -14,6 +14,7 @@ class DefaultController extends Controller
      */
     public function indexAction(Request $request)
     {
+        dump($_SESSION['rol']);
         // replace this example code with whatever you need
         return $this->render('default/index.html.twig', [
             'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
