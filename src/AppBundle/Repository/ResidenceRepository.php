23b526203d331e42be545899405457ffa8b37106
cancinos <cancinos7guatemala@gmail.com>
2017-11-17 20:28:04 -0600

    Repositorio de residencias

--- src/AppBundle/Repository/ResidenceRepository.php
+++ src/AppBundle/Repository/ResidenceRepository.php
@@ -19,7 +19,7 @@ class ResidenceRepository
     public function loadResidenceDataByIdResidence($id_residence)
     {
         $queryBuilder = $this->createQueryBuilder('r')
-                             ->select('r.code_residence, r.address, r.telephone, r.sector')
+                             ->select('r.code_residence, r.resident_code, r.address, r.telephone, r.sector')
                              ->where('r.id_residence = :id_residence')
                              ->setParameter('id_residence', $id_residence)
                              ->getQuery()
@@ -35,6 +35,7 @@ class ResidenceRepository
     public function createResidence($residence_code, $telephone, $address, $sector){
 
         $residence = new Residence($residence_code,
+                                   $resident_code,
                                    $telephone,
                                    $address,
                                    $sector
@@ -44,6 +45,65 @@ class ResidenceRepository
         $em->persist($residence);
         $em->flush();
         return $residence;
+    }
+
+    /**
+     * Validación de existencia de la residencia
+     */
+    public function Exist($code)
+    {
+        $query = $this->createQueryBuilder('p')
+                      ->from('AppBundle:residence','r')
+                      ->where('r.residence_code = :code')
+                      ->setParameter('code', $code)
+                      ->getQuery();
+
+        if(empty($query->getResult())){ return false; }
+        return true;
+    }
+
+    /**
+     * Método que permite obtener todas las residencias en
+     * base de datos 
+     */
+    public function GetAll()
+    {
+        $em = $this->getEntityManager();
+
+        $query = $em->createQuery('SELECT u.residence_code, u.resident_code, u.telephone, u.address, u.sector
+                                   FROM AppBundle\Entity\Residence u 
+                                   ORDER BY u.residence_code ASC, u.resident_code ASC'
+                                 );
+
+        $residents = $query->getResult();
+
+        return $residents;
+    }
+
+    /**
+     * Obtiene el Residente por su codigo
+     */
+    public function Get_by_Code($code)
+    {
+        return $this->createQueryBuilder('r')
+                    ->where('r.residence_code = :code')
+                    ->setParameter(':code', $code)
+                    ->getQuery()
+                    ->getOneOrNullResult();
+    }
+
+    /**
+     * Eliminación de la base de datos de residencias
+     */
+    public function DelResidence($resid)
+    {
+       $em= $this->getEntityManager();
+       $em->remove(
+           $this->Get_by_Code(
+               $resid
+            )
+        );
+        $em->flush();
     }
 }
 ?>
