e2679703500c2002429c1099fcef0a93ecbe20f0
Mariocc63 <mariocldrn@gmail.com>
2017-11-19 09:08:00 -0600

    Update Monthly_Pay.php

--- src/AppBundle/Entity/Monthly_Pay.php
+++ src/AppBundle/Entity/Monthly_Pay.php
@@ -39,7 +39,7 @@ class Monthly_Pay
 	 private $total;
 	 
 	 /**
-     * @ORM\Column(type="string", length=50)
+     * @ORM\Column(type="string", length=50, nullable=true)
      */
     private $description;
 	 
